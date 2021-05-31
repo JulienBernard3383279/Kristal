5fbdf5d11937e7dc5adba2b04cbe9021b54bd3e4
Julien Bernard <artessbm@gmail.com>
2021-06-01 00:04:14 +0200

    don't forget to flush the ack queue when flushing it : )

--- Source/Core/Core/Slippi/SlippiNetplay.cpp
+++ Source/Core/Core/Slippi/SlippiNetplay.cpp
@@ -349,6 +349,7 @@ unsigned int SlippiNetplayClient::OnData(sf::Packet &packet, ENetPeer *peer)
 			{
 				appendToPacket(cpac, outgoingAcksQueue.front());
 			}
+			outgoingAcksQueue.clear();
 
 			ENetPacket *epac = enet_packet_create(spac.getData(), spac.getDataSize(), ENET_PACKET_FLAG_UNSEQUENCED);
 			int sendResult = enet_peer_send(peer, 2, epac);
